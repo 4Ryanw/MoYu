bcc5e2e238683a0a8806b70cc5fc9a6d98630f4e
wenran <narnew@163.com>
2021-08-08 00:29:51 +0800

    初始化提交

---
+++ src/main/java/narnew/moyu/MoYuApplication.java
@@ -0,0 +1,13 @@
+package narnew.moyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoYuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoYuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/narnew/moyu/MoYuApplicationTests.java
@@ -0,0 +1,13 @@
+package narnew.moyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoYuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
